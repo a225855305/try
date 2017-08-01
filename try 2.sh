afa1fd90483126754b2ae169aeaa77f58b56b7fc
ted-yu <ted_yu@apple.com>
2017-08-01 14:42:37 +0800

    try cpu & mem data

---
+++ try 2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch usage.csv
+
+
+
+while [ 1 ]
+do
+	TIME1=$(date)
+	CPU=$(top -l 1 | grep "CPU usage")
+	MEM=$(top -l 1 | grep PhysMem)
+   	echo $TIME1 , $CPU , $MEM>> usage.csv
+
+done
